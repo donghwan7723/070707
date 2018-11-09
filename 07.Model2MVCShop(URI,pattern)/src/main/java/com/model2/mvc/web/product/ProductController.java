58a392ff584a034ce6d34b5aa3901c6cf737ea3c
bit <bit@DESKTOP-40MAN84>
2018-11-09 19:55:42 +0900

    최종완료

---
+++ 07.Model2MVCShop(URI,pattern)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -0,0 +1,125 @@
+package com.model2.mvc.web.product;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
+
+//==> 상품관리 Controller
+@Controller
+@RequestMapping("/product/*")
+public class ProductController{
+	
+	//Field
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	
+	public ProductController() {
+		System.out.println(this.getClass());
+	}
+	
+	@Value("#{commonProperties['pageUnit']}")
+		int pageUnit;	
+	
+	@Value("#{commonProperties['pageSize']}")
+		int pageSize;
+	
+	@RequestMapping(value = "addProduct", method=RequestMethod.POST)
+	public String addProduct (@ModelAttribute("product") Product product) throws Exception {
+	
+		System.out.println("여기는 productController addProduct");
+		//Business Logic
+		productService.addProduct(product);
+		
+		return "forward:/product/addProduct.jsp";
+	}
+		
+	@RequestMapping(value="listProduct")
+	public String listProduct(@ModelAttribute("search") Search search, Model model, HttpServletRequest request) throws Exception {
+		
+		System.out.println("여기는 productController listProduct");
+		
+		System.out.println("==========="+search);
+		
+		
+		if(search.getCurrentPage()==0) {
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		System.out.println("==========="+search);
+		
+		
+		//Business Logic 수행
+		Map<String, Object> map = productService.getProductList(search);
+		
+		Page resultPage = new Page(search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+
+		//Model과 View 연결
+		model.addAttribute("list", map.get("list"));
+		model.addAttribute("resultPage", resultPage);
+		model.addAttribute("search", search);
+		
+		System.out.println("여기는 productController listProduct 끝지점");
+		
+		return "forward:/product/listProduct.jsp";
+	}
+	
+		@RequestMapping(value="getProduct", method=RequestMethod.GET)
+		public String getProduct (@ModelAttribute("product") Product product, Model model) throws Exception{
+		
+		System.out.println("여기는 productController getProduct");
+		
+		product = productService.getProduct(product.getProdNo());
+		
+		model.addAttribute("product", product);
+		
+		return "forward:/product/getProduct.jsp"; 
+	}
+		
+		@RequestMapping(value="updateProductView", method=RequestMethod.GET)
+		public String updateProductView(@RequestParam("prodNo") int prodNo, Model model) throws Exception {
+			
+			System.out.println("여기는 productController updateProductView");
+			
+			Product product = productService.getProduct(prodNo);
+			
+			model.addAttribute("product", product);
+		
+			return "forward:/product/updateProductView.jsp";
+		}
+		
+		@RequestMapping(value="updateProduct", method=RequestMethod.POST)
+		public String updateProduct(@ModelAttribute("product") Product product, 
+									@RequestParam("menu") String menu) throws Exception {
+			
+			System.out.println("여기는 productController updateProduct");
+			
+			System.out.println("디버깅용"+product);
+			System.out.println("디버깅용"+menu);
+					
+			productService.updateProduct(product);
+			
+		
+			return "redirect:/product/getProduct?prodNo="+product.getProdNo()+"&menu="+menu;
+		}
+		
+
+
+
+}
